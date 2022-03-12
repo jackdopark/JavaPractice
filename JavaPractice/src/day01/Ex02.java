7c9ef222cce5a076bd43a504e7f54f7237316b6a
jackdopark <jackdopark8@gmail.com>
2022-03-12 21:53:13 +0900

    사칙연

---
+++ JavaPractice/src/day01/Ex02.java
@@ -0,0 +1,23 @@
+package day01;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("연습해보자");
+		System.out.print("3+5=");
+		System.out.println(3+5);
+		/*연습해보자
+		3+5=8
+		*/
+		//print'ln'은 괄호안의 내용을 출력하고 줄바꿈을 한다는 뜻이고
+		//print는 위와 다르게 괄호안의 내용을 출력하고 줄바꿈을 안한다는 뜻이다.
+		System.out.println("5+3="+5+3);
+		//사칙연산도 가능하다.
+		System.out.println("5-3="+(5-3));
+		//소괄호로 계산순서를 정해주면 문장과 계산값을 한번에 출력할 수 있다.
+		System.out.println("5*3="+5*3);
+		System.out.println("4/2="+4/2);
+	}
+
+}
