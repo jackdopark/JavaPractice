fb80b1f5d69b9773e330dfee8d9b886ec65055eb
jackdopark <jackdopark8@gmail.com>
2022-03-12 21:24:57 +0900

    jdk 11버전이후 모듈에 대한 설

---
+++ JavaPractice/src/day01/Ex01.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int x =1;
+			int y=2;
+			int r=x+y;
+			System.out.println(r);
+			// 출력값이 문자가 아닐때는 쌍따옴표가 필요없다.
+	}
+
+}
