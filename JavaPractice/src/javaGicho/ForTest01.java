4838f1609d71cd48c88c83927c4c1c28d6f23c8c
jackdopark <jackdopark8@gmail.com>
2022-03-27 12:48:47 +0900

    .

--- JavaPractice/src/javaGicho/ForTest01.java
+++ JavaPractice/src/javaGicho/ForTest01.java
@@ -4,8 +4,13 @@ public class ForTest01 {
 
 	public static void main(String[] args) {
 		//반복문
-		
-
+		int sum=0;
+		for(int i = 0; i <=10; i++ ) {
+			sum += i;
+			
+		}
+		System.out.println(sum);
+		//
 	}
 
 }
